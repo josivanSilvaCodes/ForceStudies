15ec601e620a747a0749717a739ce1c8c2db5edb
Josivan Pereira da Silva <josivan.engenharia@gmail.com>
2018-05-01 21:43:50 -0300

    Update SpaceShip3D.cs

--- SpaceShip3D.cs
+++ SpaceShip3D.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // 3D model to use togheter: https://www.4shared.com/file/aPdEsn3tee/Scaceship.html?
+// I recommend to scale it to x = 0.2, y = 0.2 and z = 0.2 (or others scale values you wish)
 
 public class SpaceShip3D : MonoBehaviour 
 {
