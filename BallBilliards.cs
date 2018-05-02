abc2b68d449031902fa2186efeca01c4d593b432
Josivan Pereira da Silva <josivan.engenharia@gmail.com>
2018-05-01 22:45:19 -0300

    Update BallBilliards.cs

--- BallBilliards.cs
+++ BallBilliards.cs
@@ -1,5 +1,6 @@
 
 
 
-// image texture to use togheter: https://www.4shared.com/file/UcUQH_P3fi/ball09.html
+// image texture to use togheter: https://www.4shared.com/photo/f5qJ0iymda/ball09.html
+// image texture to use togheter: https://www.4shared.com/photo/7JZSDHQwgm/ball13.html
 // Use spheres and a cube or plane to build the table and the balls of the game 
